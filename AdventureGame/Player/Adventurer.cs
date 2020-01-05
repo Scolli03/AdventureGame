e797bd2c3b8f80b7def707d820bc4d6fddb630a6
Shain Collins <Scolli03.sc@gmail.com>
2020-01-05 16:49:56 -0600

    Add project files.

---
+++ AdventureGame/Player/Adventurer.cs
@@ -0,0 +1,30 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventureGame.Player
+{
+    class Adventurer
+    {
+        public string Name { get; set; }
+        public int HP { get; set; }
+        public string CurrentQuest { get; set; }
+
+
+        public Adventurer(string _name)
+        {
+            Name = _name;
+            set_hp(100);
+        }
+
+        public void set_hp(int hp)
+        {
+            HP = hp;
+        }
+
+        public void set_quest(string quest_name)
+        {
+            CurrentQuest = quest_name;
+        }
+    }
+}
---
+++ AdventureGame/Program.cs
@@ -0,0 +1,26 @@
+﻿using AdventureGame.Player;
+using System;
+
+namespace AdventureGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Adventurer!\nWhat is your name?");
+
+            var name = Console.ReadLine();
+
+            Adventurer adv = new Adventurer(name.ToLower());
+
+            Console.WriteLine($"Welcome {adv.Name}!");
+
+            if (adv.CurrentQuest == null)
+            {
+
+                Console.WriteLine("Current quest: The Beginning");
+            }
+
+        }
+    }
+}
